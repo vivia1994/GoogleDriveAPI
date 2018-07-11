a28f98f30af129fe3b72facd8c7944b6a096166d
vivia1994 <leiwei183@163.com>
2018-07-11 14:53:06 +0800

    add RegisterMetadata class.

---
+++ GoogleDriveHelper.Helper/RegisterMetadata.cs
@@ -0,0 +1,11 @@
+﻿using System.Activities.Presentation.Metadata;
+
+namespace GoogleDriveHelper
+{
+    public class RegisterMetadata : IRegisterMetadata
+    {
+        public void Register()
+        {
+        }
+    }
+}
